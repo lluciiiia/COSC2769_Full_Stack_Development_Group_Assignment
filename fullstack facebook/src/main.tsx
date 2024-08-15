f0cb37fc93540c9d8de4718c71db6d78ce89f222
TaiVanNgo <vantaingo.056@gmail.com>
2024-08-15 13:01:44 +0700

    feat: offline page

--- fullstack facebook/src/main.tsx
+++ fullstack facebook/src/main.tsx
@@ -7,6 +7,7 @@ import ErrorPage from "./pages/ErrorPage.tsx";
 import Profile from "./pages/Profile.tsx";
 import { Provider } from "react-redux";
 import { store } from "./app/store.ts";
+import OfflinePage from "./pages/OfflinePage.tsx";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
   {
     path: "/profile",
     element: <Profile />,
+  },
+  {
+    path: "/offline",
+    element: <OfflinePage />,
   },
 ]);
 
---
+++ fullstack facebook/src/pages/OfflinePage.tsx
@@ -0,0 +1,15 @@
+import logo from "../assets/logo2.png";
+
+const OfflinePage = () => {
+  return (
+    <div>
+      <div className="bg-black h-10"></div>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <img src={logo} alt="" className="h-40 w-auto object-contain" />
+        <h1 className="text-6xl font-light">Offline</h1>
+        <h1 className="text-5xl font-thin">BuZznet will be back shortly </h1>
+      </div>
+    </div>
+  );
+};
+export default OfflinePage;
