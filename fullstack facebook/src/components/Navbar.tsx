377f3e88104dbc351c7dc10b258db51c6fb783d6
TaiVanNgo <vantaingo.056@gmail.com>
2024-08-15 12:41:34 +0700

    feat: create front end for Nav bar

--- fullstack facebook/src/components/Navbar.tsx
+++ fullstack facebook/src/components/Navbar.tsx
@@ -1,4 +1,43 @@
+// IMAGE IMPORT
+import logo from "../assets/logo.png";
+import notificationIcon from "../assets/notificationIcon.png";
+import profileLogo from "../assets/profileLogo.png";
+import homeLogo from "../assets/homeLogo.png";
+import groupLogo from "../assets/groupLogo.png";
+import createPostLogo from "../assets/createPostLogo.png";
+
+// ------------------------------------------------------
+
 const Navbar = () => {
-  return <div className="text-3xl bg-green-50">navbar</div>;
+  return (
+    <nav className="p-2 flex  bg-[#FFC123] items-center justify-between">
+      <div className="flex items-center gap-2">
+        <img
+          src={logo}
+          alt="BuZzNet Logo"
+          className="h-12 w-12 object-contain"
+        />
+        <h1 className="text-2xl font-bold">BuZzNet</h1>
+      </div>
+
+      <div className="flex gap-5 justify-around flex-grow max-w-xs flex-1 ">
+        <NavItem src={homeLogo} label={"Home"} />
+        <NavItem src={groupLogo} label="Group" />
+        <NavItem src={createPostLogo} label="Create Post" />
+      </div>
+
+      <div className="flex justify-between gap-5 mr-6">
+        <NavItem src={notificationIcon} label={"Notification"} />
+        <NavItem src={profileLogo} label={"Profile"} />
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
+
+const NavItem = ({ src, label }: { src: string; label: string }) => (
+  <div className="flex flex-col items-center w-full flex-1 cursor-pointer hover:underline focus:outline-none">
+    <img src={src} alt={`${label} icon`} className="h-6" />
+    <p>{label}</p>
+  </div>
+);
--- fullstack facebook/src/pages/Home.tsx
+++ fullstack facebook/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
 
+
 const Home = () => {
   return (
     <div>
       <Navbar />
+
       <h1>Home</h1>
     </div>
   );
