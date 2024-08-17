755b449b4670fa194d887130ad64bc67f7f8d64b
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-08-17 21:09:57 +0700

    feat: make the homepage scrollable while fixing nav bar

--- fullstack facebook/src/pages/Home.tsx
+++ fullstack facebook/src/pages/Home.tsx
@@ -1,22 +1,34 @@
 import Navbar from "../components/Navbar";
-import  Post  from "../components/Post.tsx";
-
-
+import Post from "../components/Post.tsx";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Navbar />
-
-      <h1>Home</h1>
-      <Post
-            profileImage="https://i.redd.it/07cowjrlyhda1.jpg"
-            profileName="Aqua konosuba"
-            postContent="Receive grades yesterday"
-            postImage="https://steamuserimages-a.akamaihd.net/ugc/1013818009148019610/187908C668F299A58F2E07A25874D7075CE5F17F/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"
-             profileLink="https://example.com/profile/Konosuba"
+      <div className="mt-[70px] flex h-full flex-col items-center gap-6 overflow-y-auto">
+        <Post
+          profileImage="https://i.redd.it/07cowjrlyhda1.jpg"
+          profileName="Aqua konosuba"
+          postContent="Receive grades yesterday"
+          postImage="https://steamuserimages-a.akamaihd.net/ugc/1013818009148019610/187908C668F299A58F2E07A25874D7075CE5F17F/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"
+          profileLink="https://example.com/profile/Konosuba"
         />
-    </div>
+        <Post
+          profileImage="https://i.redd.it/07cowjrlyhda1.jpg"
+          profileName="Aqua konosuba"
+          postContent="Receive grades yesterday"
+          postImage="https://steamuserimages-a.akamaihd.net/ugc/1013818009148019610/187908C668F299A58F2E07A25874D7075CE5F17F/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"
+          profileLink="https://example.com/profile/Konosuba"
+        />
+        <Post
+          profileImage="https://i.redd.it/07cowjrlyhda1.jpg"
+          profileName="Aqua konosuba"
+          postContent="Receive grades yesterday"
+          postImage="https://steamuserimages-a.akamaihd.net/ugc/1013818009148019610/187908C668F299A58F2E07A25874D7075CE5F17F/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"
+          profileLink="https://example.com/profile/Konosuba"
+        />
+      </div>
+    </>
   );
 };
 export default Home;
