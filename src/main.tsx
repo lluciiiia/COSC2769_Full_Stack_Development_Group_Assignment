2e308b93d2e4e49d8e6385e3c1bcd77669617e6a
TaiVanNgo <vantaingo.056@gmail.com>
2024-08-18 15:10:21 +0700

    fix: udpate the name of the directory
    I think it's better to use this way to name the directories rather than the camelCase

--- src/main.tsx
+++ src/main.tsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
         element: <Profile />,
       },
       {
-        path: "/signUp",
+        path: "/sign-up",
         element: <Signup />,
       },
       {
