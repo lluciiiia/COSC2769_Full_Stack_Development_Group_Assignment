f8125f907ff1eb204dcf490aaf907b23252dec61
TaiVanNgo <vantaingo.056@gmail.com>
2024-08-22 15:35:16 +0700

    feat: transition for navbar

--- src/components/AdminNavbar.tsx
+++ src/components/AdminNavbar.tsx
@@ -67,7 +67,7 @@ export const AdminNavItem: React.FC<AdminNavItemProps> = ({
         setActiveTab(label);
       }
     }}
-    className="flex w-full flex-1 cursor-pointer flex-col items-center hover:underline focus:outline-none"
+    className="flex w-full flex-1 cursor-pointer flex-col items-center transition-all duration-300 ease-in-out hover:scale-110 hover:underline focus:outline-none"
   >
     <img src={src} alt={`${label} icon`} className="h-6" />
     <p>{label}</p>
--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -7,7 +7,7 @@ interface NavItemProps {
 export const NavItem: React.FC<NavItemProps> = ({ src, label, onClick }) => (
   <div
     onClick={onClick}
-    className="flex w-full flex-1 cursor-pointer flex-col items-center hover:underline focus:outline-none"
+    className="flex w-full flex-1 cursor-pointer flex-col items-center transition-all duration-300 ease-in-out hover:scale-110 hover:underline focus:outline-none"
   >
     <img src={src} alt={`${label} icon`} className="h-6" />
     <p>{label}</p>
