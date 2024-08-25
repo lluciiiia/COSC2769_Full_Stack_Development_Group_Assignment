8b245c8e5d702f0460a2546d0e7340eef130a3a2
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-08-26 00:24:34 +0700

    feat!: add reactions to comments schema

--- server/models/reactions.ts
+++ server/models/reactions.ts
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 
-const reactionSchema= new mongoose.Schema({
-    userId:{type: mongoose.Schema.Types.ObjectId, ref:'User',required: true},
-    reactionType: {type: String, required : true},
-    dateCreated: { type: Date, default: Date.now }
-})
+const reactionSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  reactionType: {
+    type: String,
+    enum: ["LIKE", "LOVE", "HAHA", "ANGRY"],
+    required: true,
+  },
+  dateCreated: { type: Date, default: Date.now },
+});
 
-const Reaction = mongoose.model('Reaction',reactionSchema);
+const Reaction = mongoose.model("Reaction", reactionSchema);
 export default Reaction;
