a51224e7670e55410e247e17c59cf0ce25ba8761
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2024-08-25 14:25:00 +0700

    chore: noti and post routes

--- server/routes/postRoutes.ts
+++ server/routes/postRoutes.ts
@@ -1,18 +1,42 @@
 import express from "express";
+import Post from "../models/post";
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    console.log("GET /post hit");
-    res.json({ message: 'List of post' });
+router.get('/',async (req, res) => {
+    try{
+        const posts= await Post.find();
+        res.json(posts);
+    }catch(error){
+        console.error("Error fetching posts", error);
+        res.status(500).json({error: 'Falied to fetch posts'});
+    }
   });
   
-  router.post('/', (req, res) => {
-    res.json({ message: 'post created' });
+  router.post('/',async (req, res) => {
+    try {
+        const newPost = new Post(req.body);  
+        console.log(newPost);
+        await newPost.save();  
+        res.status(201).json({ message: 'Post created', post: newPost });
+      } catch (error: any) {
+        console.error("Error creating post:", error);
+
+        if (error.name === 'ValidationError') {
+          return res.status(400).json({ error: error.message });
+        }
+        res.status(500).json({ error: 'Failed to create post' });
+      }
   });
   
-  router.delete('/', (req, res) => {
-      console.log("DELETE /post hit");
-    res.json({ message: 'post deleted' });
+  router.delete('/:id', async (req, res) => {
+    try {
+      const postId = req.params.id;
+      await Post.findByIdAndDelete(postId);  
+      res.json({ message: 'Post deleted' });
+    } catch (error) {
+      console.error("Error deleting Post:", error);
+      res.status(500).json({ error: 'Failed to delete post' });
+    }
   });
 
   export default router;
