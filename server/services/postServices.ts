832fb7c33f95c7603109b3a0df8cecca45b8b0c4
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-08-25 20:15:54 +0700

    feat: handle createPost logic

--- server/services/postServices.ts
+++ server/services/postServices.ts
@@ -1,4 +1,6 @@
+import Group from "../models/group";
 import Post from "../models/post";
+import User from "../models/user";
 
 export const getAllPosts = async () => {
   try {
@@ -12,6 +14,23 @@ export const getAllPosts = async () => {
 
 export const createPost = async (postData: any) => {
   try {
+    // Check if creator exists
+    const user = await User.findById(postData.creatorId);
+    console.log("user: " + user);
+    if (!user) throw new Error("User not found with the provided creatorId");
+
+    // Check if group exists
+    if (postData.groupId && postData.groupId != "") {
+      const group = await Group.findById(postData.groupId);
+      console.log("group: " + group);
+
+      if (!group) throw new Error("Group not found with the provided groupId");
+
+      // Check if the user is a member of the group
+      const isMember = group.members.includes(postData.creatorId);
+      if (!isMember) throw new Error("User is not a member of the group");
+    }
+
     const newPost = new Post(postData);
     await newPost.save();
     return newPost;
