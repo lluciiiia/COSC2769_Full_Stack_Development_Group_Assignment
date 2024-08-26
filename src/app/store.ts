ec42e02e3c4ad824d4562a6bbff6833b3bc14c82
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2024-08-26 21:56:40 +0700

    feat: add register

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,14 +2,14 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
 import groupReducer from "../features/groupSlice";
 import postsReducer from "../features/postsSlice";
-
+import authReducer from "../features/authSlice";
 export const rootReducer = combineReducers({
   //write hte reducer in this function and
   //the type when using selector is AppState
   users: userReducer,
   groups: groupReducer,
   posts: postsReducer,
-
+  auth: authReducer
 });
 
 export type AppState = ReturnType<typeof rootReducer>;
