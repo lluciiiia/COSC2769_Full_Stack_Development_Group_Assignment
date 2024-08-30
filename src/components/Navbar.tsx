e7687c7e14fd3b85ed70e704bf097758337bc3e4
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2024-08-30 14:22:13 +0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
     setIsPostModalOpen(false);
   };
   const handleGroupClick = () => {
-    navigate(`/GroupList`);
+    navigate(`/groups`);
   };
 
   const handleProfoleClick = () => {
