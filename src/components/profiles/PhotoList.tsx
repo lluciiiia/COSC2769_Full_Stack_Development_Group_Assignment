deb38d21e229d0c54026641ba009489181378b5d
TaiVanNgo <vantaingo.056@gmail.com>
2024-09-03 01:01:05 +0700

    fix: Photo edit

--- src/components/profiles/PhotoList.tsx
+++ src/components/profiles/PhotoList.tsx
@@ -6,32 +6,27 @@ const PhotoList: React.FC = () => {
   const posts = useSelector((state: AppState) => state.posts.creatorPost);
   console.log(posts);
 
-  const images = posts.find((p) => !p.imageURL);
-  console.log(images);
-
   if (posts.length === 0) {
     return <h1>No photo available</h1>;
   }
 
   return (
     <div className="grid grid-cols-2 gap-4 p-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-      {posts.map((photo) => {
-        if (photo.imageURL) {
-          return (
-            <div
-              key={photo._id}
-              className="relative overflow-hidden rounded-lg shadow-md"
-            >
-              <img
-                src={photo.imageURL}
-                alt={`Photo ${photo._id}`}
-                className="h-[300px] w-[300px] object-cover"
-              />
-            </div>
-          );
-        }
-
-        return null;
+      {posts.map((post) => {
+        return post.images
+          ? post.images.map((image, index) => (
+              <div
+                key={`${post._id}-${index}`}
+                className="relative overflow-hidden rounded-lg shadow-md"
+              >
+                <img
+                  src={image}
+                  alt={`Photo ${post._id}-${index}`}
+                  className="h-[300px] w-[300px] object-cover transition-transform duration-200 hover:scale-125"
+                />{" "}
+              </div>
+            ))
+          : null;
       })}
     </div>
   );
