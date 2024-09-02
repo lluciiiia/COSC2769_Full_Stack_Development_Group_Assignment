d621facaa090e3d08c47172e11562c8df9a58cb2
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2024-09-03 02:10:22 +0700

    chore: set-up admin

--- server/models/admin.ts
+++ server/models/admin.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const adminSchema= new mongoose.Schema({
-    username: {type: String},
-    password: {type: String},
+    username: {type: String, required: true},
+    password: {type: String,  required: true},
 })
 
 const Admin= mongoose.model("Amin", adminSchema);
